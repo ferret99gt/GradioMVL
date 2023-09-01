0cf0c2e2a234b3594fb6a955ed17b9edff4bb3f1
ferret99gt <ferret99gt@gmail.com>
2023-08-31 23:25:20 -0400

    Why not? Allow finer tuning.

--- mvl.py
+++ mvl.py
@@ -189,7 +189,7 @@ def start():
         with gr.Row():
             inputDrop = gr.Dropdown(choices=inputNames, label="Input Device");
             outputDrop = gr.Dropdown(choices=outputNames, label="Output Device");
-            latencySlider = gr.Slider(50, max_input_latency, label="Input latency (milliseconds)", step=50, value=300);
+            latencySlider = gr.Slider(50, max_input_latency, label="Input latency (milliseconds)", step=10, value=300);
             crossfadeDrop = gr.Dropdown(choices=crossFadeNames, value="linear", label="Crossfade Method");
         with gr.Row():
             voiceDrop = gr.Dropdown(choices=voices, value="yara", label="Voice File");
